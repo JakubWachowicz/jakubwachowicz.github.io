227ebc6e4a3c401a4c9d66f4413c2cdeec15fd02
Jakub Wachowicz <wacho@THINKTWICE>
2024-10-30 00:42:54 +0100

    Navigation update

--- src/components/navigation_bar.js
+++ src/components/navigation_bar.js
@@ -1,8 +1,7 @@
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-black bg-opacity-70 text-white py-4 px-8 flex justify-between items-center z-20">
-      <div className="text-xl font-bold">Jakub Wachowicz</div>
-      <div className="flex space-x-6">
+    <nav className="fixed items-end top-0  w-full bg-black bg-opacity-70 text-white py-4 px-8 flex justify-end items-center z-20">
+      <div className="items-end flex space-x-6">
         <a href="#home" className="hover:text-gray-400 transition duration-200">Home</a>
         <a href="#about" className="hover:text-gray-400 transition duration-200">About</a>
         <a href="#projects" className="hover:text-gray-400 transition duration-200">Projects</a>
